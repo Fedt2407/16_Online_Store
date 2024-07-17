7fce1a5f5cf2a3b743f3930f7e66870b72e44c41
Fedt2407 <federico.tognetti@gmail.com>
2024-07-17 10:41:45 +0200

    DB creation

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# DB setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(200), nullable=False)
+
+# Create all tables
+with app.app_context():
+    db.create_all()
+
+
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def home():
+    products = Product.query.all()
+    return render_template('index.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
