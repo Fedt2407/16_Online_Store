429978f236305183bd09a697d4f95d6d22f657bb
Fedt2407 <federico.tognetti@gmail.com>
2024-07-17 15:16:14 +0200

    implemented register and login html

--- main.py
+++ main.py
@@ -47,5 +47,13 @@ def home():
 def contacts():
     return render_template('contacts.html')
 
+@app.route('/register') 
+def register():
+    return render_template('register.html')
+
+@app.route('/login') 
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
